bf19a1a7f1905425c3bfaca3d3a250ea37741be2
Henry Parker <henryparker594@gmail.com>
2018-11-17 18:38:08 -0500

    add user to socket

--- react-ui/src/component/Layout.js
+++ react-ui/src/component/Layout.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
-
+import {USER_CONNECTED,LOGOUT} from '../Events';
+import LoginForm from './LoginForm';
 const socketUrl = "localhost:3231"
 class Layout extends Component {
     constructor(props){
         super(props);
         this.state = {
             socket:null,
+            user:null
         }
     }
     componentWillMount(){
@@ -19,9 +21,23 @@ class Layout extends Component {
         })
         this.setState({socket})
     }
+
+    setUser = (user)=>{
+        const {socket} = this.state
+        socket.emit(USER_CONNECTED);
+        this.setState({user})
+    }
+
+    logout =()=>{
+        const {socket} = this.state
+        socket.emit(LOGOUT)
+        this.setState({user:null})
+    }
   render() {
+      const socket = this.state.socket
     return (
-      <div>
+      <div className="container">
+          <LoginForm socket ={socket} setUser={this.setUser}/>
         <h1>Layout</h1>
         
       </div>
--- server/SocketManager.js
+++ server/SocketManager.js
@@ -1,5 +1,38 @@
-const io = require('./index.js').io
+const io = require('./index.js').io;
+const {VERIFY_USER,USER_CONNECTED,LOGOUT} = require('../react-ui/src/Events');
+const {createUser} = require('../react-ui/src/Factories');
 
+let connectedUser = {}
+
+addUser = (userList,user) =>{
+    let newList = Object.assign({},userList)
+    newList[user.name] = user
+    return newList
+}
+removeUser = (userList,username) =>{
+    let newList = Object.assign({},userList)
+    delete newList[username]
+    return newList
+}
+
+isUser = (userList,username) =>{
+    return username in userList
+}
 module.exports = function(socket){
     console.log(`Socket ID: ${socket.id}`)
+
+    socket.on(VERIFY_USER,(nickname,callback)=>{
+        if(isUser(connectedUser,nickname)){
+            callback({isUser:true, user:null})
+        }else{
+            callback({isUser:false,user:createUser({name:nickname})})
+        }
+    })
+
+    socket.on(USER_CONNECTED, (user)=>{
+        connectedUser = addUser(connectedUser,user)
+        socket.user = user
+        io.emit(USER_CONNECTED,connectedUser)
+        console.log(connectedUser);
+    })
 }
