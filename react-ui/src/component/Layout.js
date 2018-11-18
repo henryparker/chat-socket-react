521f08ed774397b55369f4e1789f6ed0c118b86b
Henry Parker <henryparker594@gmail.com>
2018-11-17 23:17:07 -0500

    side Bar

--- react-ui/src/component/Layout.js
+++ react-ui/src/component/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 import {USER_CONNECTED,LOGOUT} from '../Events';
 import LoginForm from './LoginForm';
+import  ChatContainer from './ChatContainer';
 const socketUrl = "localhost:3231"
 class Layout extends Component {
     constructor(props){
@@ -34,11 +35,15 @@ class Layout extends Component {
         this.setState({user:null})
     }
   render() {
-      const socket = this.state.socket
+      const {socket,user} = this.state
+      
     return (
       <div className="container">
-          <LoginForm socket ={socket} setUser={this.setUser}/>
-        <h1>Layout</h1>
+        {!user ?
+        <LoginForm socket ={socket} setUser={this.setUser}/>
+        :
+        <ChatContainer socket={socket} user={user} logout={this.logout}/>}
+    
         
       </div>
     );
